f2f0c84e22bf014c2912cc5e943828b318951d8c
Finn Hodgkin <finnhodgkin@gmail.com>
2017-01-09 14:39:01 +0000

    add keyboard controls

--- js/animate.js
+++ js/animate.js
@@ -7,36 +7,49 @@
   var pic = document.getElementById('pic');
   var current = '';
 
+  function imageFormatter (num) {
+    num = ('00' + (num)).slice(-3);
+    return 'url("images/' + (overwrites[num] || num + '.png') + '")';
+  }
+
+  function nextImage (cur, dir) {
+    cur += dir;
+    // if last or first box, cancel image change
+    if (dir === -1 && cur < 0) { cur = boxes.length - 1; }
+    if (dir === 1 && cur > boxes.length - 1) { cur = 0; }
+    return cur;
+  }
+
   boxes.forEach(function (e) {
     e.addEventListener('click', function () {
       gallery.className += ' on';
       current = boxes.indexOf(e);
-      pic.style.backgroundImage = e.style.backgroundImage;
+      pic.style.backgroundImage = imageFormatter(current);
     });
+  });
+
+  window.addEventListener('keyup', function (e) {
+    if (e.keyCode === 37) { current = nextImage(current, -1); } // left
+    if (e.keyCode === 39) { current = nextImage(current, 1); }  // right
+    pic.style.backgroundImage = imageFormatter(current);
+    if (e.keyCode === 27) {
+      gallery.className = gallery.className.replace(' on', '');
+    }
   });
 
   controls.forEach(function (e) {
     e.addEventListener('click', function () {
       var direction = this.id === 'rightarrow' ? 1 : -1;
-      var picture = '';
-      current = current + direction;
-      // if last or first box, cancel image change
-      if (direction === -1 && current < 0) {
-        current = boxes.length - 1;
-      }
-      if (direction === 1 && current > boxes.length - 1) {
-        current = 0;
-      }
+      current = nextImage(current, direction);
       // change picture
-      picture = ('00' + current).slice(-3);
-      console.log(current);
-      pic.style.backgroundImage = 'url("images/' + (overwrites[picture] || picture + '.png') + '")';
+      pic.style.backgroundImage = imageFormatter(current);
     });
   });
 
   gallery.addEventListener('click', function (e) {
     // if controls then don't close gallery
     if (e.target !== this && e.target !== pic) { return; }
+    // close gallery
     gallery.className = gallery.className.replace(' on', '');
   });
 })();
